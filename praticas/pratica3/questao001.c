2823c920418b6b79b049a2a972e107af51b83eb2
mariaclarainacio <160738075+mariaclarainacio@users.noreply.github.com>
2024-04-24 13:48:38 +0000

    Aula06 Pratica3

---
+++ praticas/pratica3/questao001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+  int numero,par,impar;
+  printf("Digite um numero para saber se ele e par ou impar\n");
+  int deu_certo=scanf("%d", &numero);
+  if (numero % 2 == 0)
+    printf("O numero %d eh par d--(- _ -)z ", numero);
+}else{
+
+  printf("O numero %d eh impar p-- (- W -)", numero);
+}else{
+  
+return 0;
+}
