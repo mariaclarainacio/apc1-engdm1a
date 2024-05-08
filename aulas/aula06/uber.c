5e22d993a7791c75bd9280df34118dfa748a7ca1
mariaclarainacio <160738075+mariaclarainacio@users.noreply.github.com>
2024-05-08 13:48:16 +0000

    codigos de aula 6 e 7 + praticas 3

---
+++ aulas/aula06/uber.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+// int avaliacao= 0;
+
+//   printf("Entre com a avaliacao de 1 a 5: ");
+//   int leu_certo = scanf("%i", &avaliacao);
+  
+//   if (avaliacao == 1) {
+//     printf("O motorista ganhou *\n");
+//  } else if (avaliacao == 2) {
+//   printf("O motorista ganhou **\n");
+// } else if (avaliacao == 3) {
+//   printf("O motorista ganhou ***\n");
+// } else if (avaliacao == 4) {
+//   printf("O motorista ganhou ****\n");
+// } else if (avaliacao == 5) {
+//   printf("O motorista ganhou *****\n");
+// } else {
+//   printf("Avaliacao invalida. Tente novamente");
+  
+// }
+   switch(avaliacao) {
+      case 1:
+      printf("O motorista ganhou *\n");
+      case 2:
+      printf("O motorista ganhou **\n");
+      case 3:
+       printf("O motorista ganhou ***\n");
+      case 4:
+       printf("O motorista ganhou ****\n");
+      case 5:
+       printf("O motorista ganhou *****\n");
+
+        
+  return 0;
+}
